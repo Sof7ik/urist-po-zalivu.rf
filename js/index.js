c2476ba1dfaed448cd3ac13abf64d11eef976196
Leo <bychkov.l47@mail.ru>
2021-12-02 02:04:32 +0300

    adaptive

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 function openFAQ(e)
 {
-    console.log(e.currentTarget);
-
     const currTarget = e.currentTarget.closest('.faq-item');
 
     const answerWrapper = currTarget.querySelector('.faq-item__answer-wrapper');
@@ -11,30 +9,76 @@ function openFAQ(e)
     {
         currTarget.classList.remove('opened');
         answerWrapper.style.height = `0px`;
-
-        console.log('close')
     }
     else
     {
-        console.log('open')
-
         currTarget.classList.add('opened');
         answerWrapper.style.height = `${answerText.clientHeight}px`;
     }
 }
 
-
 document.addEventListener("DOMContentLoaded", e => {
     const casesSlider = new Swiper(".cases-slider", {
         // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
         slidesPerView: 3,
         spaceBetween: 20,
         speed: 800,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                navigation: {
+                    nextEl: '.swiper-button-next__mobile',
+                    prevEl: '.swiper-button-prev__mobile',
+                },
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+                navigation: {
+                    nextEl: '.swiper-button-next__desktop-cases',
+                    prevEl: '.swiper-button-prev__desktop-cases',
+                },
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            }
+        }
     });
+
+    const pricingSlider = new Swiper(".pricing-mobile-slider", {
+        navigation: {
+            nextEl: '.swiper-button-next__pricing',
+            prevEl: '.swiper-button-prev__pricing',
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        speed: 400,
+        freeMode: {
+            enabled: true,
+        },
+        centeredSlides: false
+    });
+
+    const paymentSlider = new Swiper(".mobile-payments-slider", {
+        navigation: {
+            nextEl: '.swiper-button-next__payments',
+            prevEl: '.swiper-button-prev__payments',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                initialSlide: 1,
+            },
+            651: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+                initialSlide: 0,
+            }
+        }
+    })
 
     document.querySelectorAll('.faq-item__title-wrapper').forEach(elem => elem.addEventListener('click', openFAQ))
 })
